ced132f2ed21b305e579e60cd12d296ff1ea911e
aniruddha-chakraborty <aniruddha.app@gmail.com>
2016-02-02 16:46:17 +0600

    updated infomation

--- js/main.js
+++ js/main.js
@@ -60,6 +60,5 @@ var newRoutine = Object.create(data);
 if (getHours < 19) {
     newRoutine.routine(getDay);
 } else {
-    var getDay = --getDay;
-    newRoutine.routine(getDay);
+    newRoutine.routine(--getDay);
 }
